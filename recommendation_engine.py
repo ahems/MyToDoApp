7bb72e47a0db128c0e66926c55f6a81e83f289e4
Adam Hems <adamhems@yahoo.com>
2024-05-15 19:23:35 +0000

    Azure Version

--- recommendation_engine.py
+++ recommendation_engine.py
@@ -1,27 +1,26 @@
+import os
 import json
 import asyncio
-import semantic_kernel as sk
 from services import Service
 from openai import AzureOpenAI
-from dotenv import dotenv_values
 
 class RecommendationEngine:
-    def __init__(self):
-        config = dotenv_values(".env")
+       def __init__(self):
+        self.deployment = os.environ.get("AZURE_OPENAI_DEPLOYMENT_NAME", '')
+        api_key = os.environ.get("AZURE_OPENAI_API_KEY", '')
+        endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT", '')
+        use_open_ai = os.environ.get("USE_AZURE_OPENAI", 'True')
 
         #uses the USE_AZURE_OPENAI variable from the .env file to determine which AI service to use
         #False means use OpenAI, True means use Azure OpenAI
-        selectedService = Service.AzureOpenAI if config.get("USE_AZURE_OPENAI") == "True" else Service.OpenAI
-
+        selectedService = Service.AzureOpenAI if use_open_ai == "True" else Service.OpenAI
         if selectedService == Service.AzureOpenAI:
-            deployment, api_key, endpoint = sk.azure_openai_settings_from_dot_env()
-            self.deployment = deployment
             self.client = AzureOpenAI(azure_endpoint = endpoint, 
                         api_key=api_key,  
                         api_version="2024-02-15-preview"
                         )
         else:
-            raise Exception("OpenAI not implemented")  
+            raise Exception("OpenAI not implemented")     
 
 
     async def get_recommendations(self, keyword_phrase, previous_links_str=None):
